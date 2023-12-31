363c8889982cfa0af67a25592879a909b5be9e32
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-12-31 02:52:57 -0800

    automating script fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # configuration changes automation using Puppet
 
-file { 'etc/ssh/ssh_config': 
+file { 'ect/ssh/ssh_config': 
 	ensure => present,
 	content => "
 		#SSH client configuration
