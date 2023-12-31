c91b7529bc63beba0f41e598a435371b5f00cbde
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-12-31 02:49:40 -0800

    automating config process using Puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# configuration changes automation using Puppet
+
+file { 'etc/ssh/ssh_config': 
+	ensure => present,
+	content => "
+		#SSH client configuration
+		host*
+		IdentityFile ~/.ssh/school
+		PasswordAuthentication no
+		"
+}
